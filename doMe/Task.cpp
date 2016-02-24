cb9a83746070bf25de0d7667a0ad2d651ee01ed3
jw <None>
2016-02-24 15:34:02 +0800

    Task app completed

--- doMe/Task.cpp
+++ doMe/Task.cpp
@@ -1,7 +1,60 @@
 #include "Task.h"
 
+std::string Task::getName(){
+	return _name;
+}
+	
+int Task::getDate1(){
+	return _date1;
+}
+	
+int Task::getDate2(){
+	return _date2;
+}
 
+int Task::getTime1(){
+	return _time1;
+}
+
+int Task::getTime2(){
+	return _time2;
+}
+std::string Task::getLocation(){
+	return _location;
+}
+
+void Task::setName(std::string newName){
+	_name = newName;
+}
+
+void Task::setDate1(int newDate){
+	_date1 = newDate;		
+}
+
+void Task::setDate2(int newDate){
+	_date2 = newDate;
+}
+
+void Task::setTime1(int newTime){
+	_time1 = newTime;
+}
+
+void Task::setTime2(int newTime){
+	_time2 = newTime;
+}
+
+void Task::setLocation(std::string newLocation){
+	_location = newLocation;
+}
+
+/*
+*  Default constructor and destructor
+*/
 Task::Task(string _name, int date1, int date2, int time1, int time2, string location) {
+	_name = "";
+	_date1 = _date2 =0;
+	_time1 = _time2 =0;
+	_location = "";
 }
 
 Task::~Task(void) {
