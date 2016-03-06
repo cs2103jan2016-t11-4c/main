41d8868b16e02ce0be2b12c6de1648eff8543f95
Michael <unknownwerty@gmail.com>
2016-03-06 21:17:27 +0800

    Added COOL WELCOME MESSAGE JUST FOR FUN ONLY

--- doMe/Logic.cpp
+++ doMe/Logic.cpp
@@ -15,7 +15,28 @@ Logic::Logic() {
 void Logic::setEnvironment() {
 	//	vectorToTaskList(_storage->retrieveData(_settings->getSaveDirectory());
 	_settings->loadSettings();
-    display(); //initial display
+
+	string space = "               ";
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << space; cout << "                   Welcome to" << endl; 
+	cout << space; cout << "      _         __  __                          " << endl;
+	cout << space; cout << "     | |       |  \\/  |                         " << endl;
+	cout << space; cout << "   __| |  ___  | \\  / |  ___     ___ __  __ ___ " << endl;
+	cout << space; cout << "  / _` | / _ \\ | |\\/| | / _ \\   / _ \\  \\/ // _ \\" << endl;
+	cout << space; cout << " | (_| || (_) || |  | ||  __/ _|  __/ >  <|  __/" << endl;
+	cout << space; cout << "  \\__,_| \\___/ |_|  |_| \\___|(_)\\___|/_/\\_\\___|" << endl;
+
+	cout << endl;
+	cout << space; cout << "           <Press Enter to continue>" << endl;
+	string command;
+	getline(cin, command);
+
+	display(); //initial display
 }
 void Logic::displayWelcomeMessage() {
 	_UI->printNotificationWelcome();
