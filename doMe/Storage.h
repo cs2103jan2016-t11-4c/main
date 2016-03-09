af0e7973da64032b48337b7041e17f902d99a548
Joan Toh Tze Siew <joantts@gmail.com>
2016-03-09 10:52:31 +0800

    Overwrite old code with new ones

--- doMe/Storage.h
+++ doMe/Storage.h
@@ -11,25 +11,37 @@
 using namespace std;
 
 // Storage class simply:
-//	1) holds the directory or file address of chosen .txt file as a private member attribute, that can be changed via
-//	   changeLocation() member function. 
-//	2) extracts content from user-specified .txt file database into a string vector for the usage of other classes, by
+//	1) extracts content from todo.txt*** file in user-specified directory into a string vector for the usage of other classes, by
 //	   returning the string vector from retrieveData()
-//	3) overwrites the .txt file database content with that of the string vector passed into the parameter of saveData()
+//	2) overwrites the todo.txt file content with that of the string vector passed into the parameter of saveData()
+//	
+//	***If todo.txt does not exist yet, indicates user is using for the first time, and todo.txt will be created after first entry
+//	or operation in given directory.
 
 class Storage {
 private:
-	//string _location;
+	string _location;
+	string _txtFileName = "todo.txt";
 	// to check whether file is already created, or physically available, in given directory
 	bool _isFileCreated;
 
 public:
+	// initialise _isFileCreated as false
 	Storage();
+	
+	// concatenate directory with default _txtFileName to get exact target .txt
+	// file destination (_location) such that fstream can open it effectively
+	// also, a setter function for file destination changes
+	void setLocation(string);
 
-	vector<string> getExistingData(string saveDirectory);
-	void saveUpdatedData(vector<string>, string saveDirectory);
-	void changeLocation(string);
+	// extract data from target .txt file and return as string vector
+	vector<string> retrieveData(string);
+
+	// save current string vector into target .txt file
+	void saveData(vector<string>, string);
+
 };
 
 #endif
+
 
