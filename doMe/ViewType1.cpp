3d04eefaef9b70e3172024c122919f1e69b95e61
Chang <peichanghong2@gmail.com>
2016-03-10 10:58:39 +0800

    Resolve viewtype 1 debug error

--- doMe/ViewType1.cpp
+++ doMe/ViewType1.cpp
@@ -52,7 +52,7 @@ string ViewType1::getTimeTaskString(int time) {
     string timeString;
 
     if(time >= 0) {
-        if(time >= TIME_MIDDAY) {
+        if(time > TIME_MIDDAY) {
             time = time - TIME_MIDDAY;
             timeString = integerToString(time);
             timeString = timeString + MESSAGE_PM;
