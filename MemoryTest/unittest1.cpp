4570e346432c50d90ae4dc035d311d9626b84fc2
Joan Toh Tze Siew <joantts@gmail.com>
2016-04-11 20:21:19 +0800

    unit test for storage class

---
+++ MemoryTest/unittest1.cpp
@@ -0,0 +1,68 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include "Storage.h"
+#include "Storage.cpp"
+#include "Exception_FileCannotOpen.h"
+#include "Exception_FileCannotOpen.cpp"
+
+#include <string>
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace MemoryTest
+{		
+	TEST_CLASS(StorageTest)
+	{
+	public:
+
+		TEST_METHOD(RetrieveTest)
+		{
+			Storage* storage = Storage::getInstance();
+			int totalLines = 4;
+			string readDirectory = "storagetestread.txt";
+			vector<string> actualData = storage->retrieveData(readDirectory);
+
+			string expectedData[] = {
+				"This is a test.",
+				"Line 2",
+				"Line 3",
+				"This is the final line."
+			};
+
+			Assert::AreEqual(totalLines, (int)actualData.size());
+
+			for (int i = 0; i < totalLines; i++) {
+				Assert::AreEqual(expectedData[i], actualData[i]);
+			}
+		}
+
+		TEST_METHOD(SaveTest)
+		{
+			// RetrieveTest must pass for SaveTest to be valid. 
+			Storage* storage = Storage::getInstance();
+			int totalLines = 4;
+			string writeDirectory = "storagetestwrite.txt";
+			vector<string> actualData;
+
+			string expectedData[] = {
+				"This is a test.",
+				"Line 2",
+				"Line 3",
+				"This is the final line."
+			};
+			vector<string> newData(expectedData, expectedData + sizeof(expectedData) / sizeof(string));
+
+			storage->saveData(newData, writeDirectory);
+			actualData = storage->retrieveData(writeDirectory);
+
+			Assert::AreEqual(totalLines, (int)actualData.size());
+
+			for (int i = 0; i < totalLines; i++) {
+				Assert::AreEqual(expectedData[i], actualData[i]);
+			}
+		}
+	};
+}
