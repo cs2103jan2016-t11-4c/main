703de6668ad30093af18b0eaf816f020cfb66971
Chang <peichanghong2@gmail.com>
2016-04-13 14:27:55 +0800

    Enhancement to UI

--- doMe/ViewType.cpp
+++ doMe/ViewType.cpp
@@ -284,7 +284,9 @@ string ViewType::getTimeTaskString(int time) {
 
     if(time >= 0) {
         if(time > TIME_MIDDAY) {
-            time = time - TIME_MIDDAY;
+            if(time > TIME_MIDDAY + 100) {
+                time = time - TIME_MIDDAY;
+            } 
             timeString = integerToString(time);
             timeString = timeString + MESSAGE_PM;
         } else {
